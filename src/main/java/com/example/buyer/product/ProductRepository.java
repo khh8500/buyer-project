eca643968786467ff03f49fbef329a92ea4a1a6c
khh <khh8500@naver.com>
2024-04-30 15:29:42 +0900

    Product JPQL 수정

--- src/main/java/com/example/buyer/product/ProductRepository.java
+++ src/main/java/com/example/buyer/product/ProductRepository.java
@@ -15,14 +15,14 @@ public class ProductRepository {
 
     // 상품 상세보기
     public Product findById(Integer id) {
-        Query query = em.createNativeQuery("select * from product_tb where id=?", Product.class);
-        query.setParameter(1, id);
+        Query query = em.createQuery("select p from Product p where id=:id", Product.class);
+        query.setParameter("id", id);
         return (Product) query.getSingleResult();
     }
 
     // 상품 목록보기
     public List<Product> findAll() {
-        Query query = em.createNativeQuery("select * from product_tb order by id desc", Product.class);
+        Query query = em.createQuery("select p from Product p order by id desc", Product.class);
         return query.getResultList();
     }
 }
