656e22c7da6fc5f758e70aeab635272ec1be1160
khh <khh8500@naver.com>
2024-04-30 15:25:26 +0900

    User JPQL 수정

--- src/main/java/com/example/buyer/user/UserRepository.java
+++ src/main/java/com/example/buyer/user/UserRepository.java
@@ -14,9 +14,10 @@ public class UserRepository {
 
     // 로그인하기
     public User findByUserIdAndPassword(UserRequest.LoginDTO reqDTO) {
-        Query query = em.createNativeQuery("select * from user_tb where user_id=? and password=?", User.class);
-        query.setParameter(1, reqDTO.getUserId());
-        query.setParameter(2, reqDTO.getPassword());
+        Query query = em.createQuery(
+                "select u from User u where u.userId=:userId and u.password=:password", User.class);
+        query.setParameter("userId", reqDTO.getUserId());
+        query.setParameter("password", reqDTO.getPassword());
 
         User user = (User) query.getSingleResult();
         return user;
