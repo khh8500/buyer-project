4006fe8407d9871ad6a73943d7cd883edfa1ae7a
khh <khh8500@naver.com>
2024-05-10 20:45:01 +0900

    장바구니 선택한 상품 삭제 미완

--- src/main/java/com/example/buyer/cart/CartController.java
+++ src/main/java/com/example/buyer/cart/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -18,9 +19,18 @@ public class CartController {
     private final CartService cartService;
     private final HttpSession session;
 
+//    // 장바구니 선택한 상품 삭제하기
+//    @PostMapping("/deleteSelectedItems")
+//    public String deleteSelectedItems(@RequestBody List<Integer> ids) {
+//        for (Integer id : ids) {
+//            cartService.deleteCart(id);
+//        }
+//        return "redirect:/cart-form";
+//    }
+
     // 장바구니 삭제하기
     @PostMapping("/cart/{id}/delete")
-    public String cartDelete(@PathVariable Integer id){
+    public String cartDelete(@PathVariable Integer id) {
         cartService.deleteCart(id);
 
         return "redirect:/cart-form";
@@ -28,7 +38,7 @@ public class CartController {
 
     // 장바구니 중복 체크 및 수량 업데이트해서 담기
     @PostMapping("/cart")
-    public String cart(CartRequest.SaveDTO reqDTO){
+    public String cart(CartRequest.SaveDTO reqDTO) {
 
         User sessionUser = (User) session.getAttribute("sessionUser");
         Integer sessionUserId = sessionUser.getId();
@@ -50,7 +60,7 @@ public class CartController {
 //    }
 
     @GetMapping("/cart-form")
-    public String cartForm(HttpServletRequest request){
+    public String cartForm(HttpServletRequest request) {
 
         User sessionUser = (User) session.getAttribute("sessionUser");
         List<CartResponse.SaveDTO> cartList = cartService.findAll(sessionUser.getId());
